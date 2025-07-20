34a4edabd2a06e1fce2069517980965fec9bb74e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2025-07-20 20:21:44 +0100

    feat: add London magnetic field

--- 01_sensor_calibration_ekf.py
+++ 01_sensor_calibration_ekf.py
@@ -1,19 +1,26 @@
 from ahrs.filters import EKF
+from ahrs.utils import WMM
+import numpy as np
 
 from lib.utils import remove_gravity, load_from_dir, frequency_from_time, plot_imu_data, gyr_convert_deg_to_rads, \
     acc_convert_mg_to_mps2, mag_convert_gauss_to_mt
 
-LONDON_MAG_ENU = [-0.0021, 0.402, -0.916]
+LONDON_LATITUDE = 51.15
+LONDON_LONGITUDE = 0.12
+LONDON_HEIGHT = 0.01
 
-common_time, mag, acc, gyro = load_from_dir("./devices/E0A8AD21/gyr_calibration/xrot/")
+common_time, mag, acc, gyro = load_from_dir("./devices/E0A8AD21/gyr_calibration/zrot/")
 
 gyro = gyr_convert_deg_to_rads(gyro)
 acc = acc_convert_mg_to_mps2(acc)
 mag = mag_convert_gauss_to_mt(mag)
-# default values [0.3**2, 0.5**2, 0.8**2]
+
+wmm = WMM(latitude=LONDON_LATITUDE, longitude=LONDON_LONGITUDE, height=LONDON_HEIGHT)
+magnetic_ref = np.array([wmm.Y, wmm.X, -wmm.Z])
+
 filter = EKF(gyr=gyro, acc=acc, mag=mag, frequency=frequency_from_time(common_time),
              frame='ENU',
-             magnetic_ref=LONDON_MAG_ENU,
+             magnetic_ref=magnetic_ref,
              var_gyr=0.5**2,
              var_acc=0.1**2,
              var_mag=0.8**2
